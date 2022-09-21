be4b621dd435f9d6910861eb901733fd9c8632ab
malle <malle@DESKTOP-IPP4SD0>
2022-09-21 12:39:28 +0530

    commit-2

---
+++ Treebo1_Societymgt_Selenium_Framework/src/test/java/org/Treebo1/practice/Ajio.java
@@ -0,0 +1,14 @@
+package org.Treebo1.practice;
+
+import org.testng.reporters.jq.Main;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Ajio {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		//comment
+		
+	}
+
+}
